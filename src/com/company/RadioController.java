68dbf16f852440f1f8c3e9e9eb309596486daa92
ximelh <ximenaloarca@gmail.com>
2022-01-18 21:02:00 -0600

    Implementación de métodos

--- src/com/company/RadioController.java
+++ src/com/company/RadioController.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class RadioController implements Radio {
     private boolean onOff; // indica el estado del radio true-> encendido, false-> apagado
     private boolean frequency; // indica la frecuencia del radio true->am false->fm
     private double[] favStationAM; // donde se guardan las estaciones favoritas del usuario AM
     private double[] favStationFM; // donde se guardan las estaciones favoritas del usuario FM
-    private double[] stationsAM; // donde se guardaran todas las estaciones de radio de la frecuencia AM
-    private double[] stationsFM; // donde se guardaran todas las estaciones de radio de la frecuencia FM
+    private ArrayList<Double> stationsAM; // donde se guardaran todas las estaciones de radio de la frecuencia AM
+    private ArrayList<Double> stationsFM; // donde se guardaran todas las estaciones de radio de la frecuencia FM
     private double currentStation; // donde se almacena la estación actual
 
     public RadioController() {
@@ -15,15 +17,35 @@ public class RadioController implements Radio {
         this.currentStation = 0;
         this.favStationAM = new double[12];
         this.favStationFM  = new double[12];
+        this.stationsAM = new ArrayList<>();
+        this.stationsFM = new ArrayList<>();
+        setstationsAMFM();
     }
     
+
+     /**
+     * Realiza el autollenado de las estaciones de radio disponibles.
+     */
+    private void setstationsAMFM() {        
+        double i=530.0;
+        stationsAM.add(i); // se agrega la primera radio
+        do {
+            stationsAM.add(i+10);
+        } while(i<=1610.0);
+
+        i=87.9;
+        stationsFM.add(i); // se agrega la primera radio
+        do {
+            stationsFM.add(i+0.2);
+        } while(i<=107.9);
+    }
 
     /**
      * @author Ximena Loarca
      */
     @Override
     public boolean isOn() {
-        return false;
+        return onOff;
     }
 
     /**
@@ -31,7 +53,12 @@ public class RadioController implements Radio {
      */
     @Override
     public void turnOnOff() {
-
+        if (onOff == false){
+            onOff = true;
+        }
+        else{
+            onOff = false;
+        }
     }
 
     /**
@@ -39,7 +66,7 @@ public class RadioController implements Radio {
      */
     @Override
     public void nextStation(boolean frequency) {
-
+        
     }
 
     /**
@@ -47,7 +74,32 @@ public class RadioController implements Radio {
     */
     @Override
     public void prevStation(boolean frequency) {
-
+        setstationsAMFM(); 
+        if(frequency==this.frequency) { // valida que sean las mismas frecuencias de radio0
+            if(this.frequency == true) { // se debe de usar el array de estaciones AM
+                int posicion=0;
+                for (double d : this.stationsAM) {
+                    if(d==this.currentStation) { break; } 
+                    posicion++; // mientras no encuentre la posicion de la estacion actual, entonces sumara a la siguiente posicion 
+                }
+                if(posicion==0) {
+                    System.out.println(this.stationsAM.get(this.stationsAM.size())); // cuando esta en la primera estacion, entonces el previo es la ultima en la lista
+                } else {
+                    System.out.println(this.stationsAM.get(posicion-1)); // de lo contrario muestra la estacion anterior 
+                }
+            } else { // se debe de usar el array de estaciones FM
+                int posicion=0;
+                for (double d : this.stationsFM) {
+                    if(d==this.currentStation) { break; } // al encontrar al valor que se igualan debe terminar el ciclo
+                    posicion++;
+                }
+                if(posicion==0) {
+                    System.out.println(this.stationsFM.get(this.stationsFM.size())); // cuando esta en la primera estacion, entonces el previo es la ultima en la lista
+                } else {
+                    System.out.println(this.stationsFM.get(posicion-1)); // de lo contrario muestra la estacion anterior 
+                }
+            }
+        }
     }
 
     /**
@@ -82,12 +134,13 @@ public class RadioController implements Radio {
         }
     }
 
-    /**
+
+     /**
      * @author Mariel Guamuche 
     */
     @Override
     public boolean getFrequency() {
-        return false;
+        return this.frequency; //retorna el valor de la frecuencia actual
     }
 
     /**
@@ -95,6 +148,10 @@ public class RadioController implements Radio {
     */
     @Override
     public void switchAMFM() {
-
+        if(this.frequency==true) { // valida el estado de la frecuencia
+            this.frequency = false; // cambia a frecuencia FM
+        } else {
+            this.frequency = true; // cambia a frecuencia AM
+        }
     }
 }
