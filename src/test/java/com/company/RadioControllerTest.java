c76ba301453a2b3cda807c5e7b100745f32091c7
Joshua Isaí Chicoj <81249233+jichicoj@users.noreply.github.com>
2022-01-19 23:04:32 -0600

    Adición de soporte Maven Creación de clase de testeo

---
+++ src/test/java/com/company/RadioControllerTest.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void isOn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void turnOnOff() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nextStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prevStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSavedStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFrequency() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void switchAMFM() {
+    }
+}
