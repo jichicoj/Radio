b4df97605d76bc6bec2d1933fef13f237019f654
gua21150 <gua21150@uvg.edu.gt>
2022-01-20 00:12:26 -0600

    Pruebas unitarias
    Se han realizado las pruebas unitarias de tres de los métodos implementados.
    Las pruebas han sido completadas correctamente.
    
    Adicionalmente se agregó la instancia de un objeto tipo RadioController.

---
+++ src/test/java/com/company/RadioControllerTest.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+
+class RadioControllerTest {
+
+    Radio radio;
+    @BeforeEach
+    void setUp() {
+        radio = new RadioController(); //realizara antes que todas las pruebas la instancia del objeto de tipo Radio
+    }
+
+    @org.junit.jupiter.api.Test
+    void isOnTest() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void turnOnOffTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nextStationTest() {        
+    }
+
+    @org.junit.jupiter.api.Test
+    void prevStationTest() {
+        radio.prevStation(radio.getFrequency());
+        radio.prevStation(radio.getFrequency());
+        assertEquals(107.7, Math.round(radio.getStation()*100)/100d);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStationTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveStationTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSavedStationTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFrequencyTest() {
+        assertEquals(false, radio.getFrequency());
+    }
+
+    @org.junit.jupiter.api.Test
+    void switchAMFMTest() {                
+        radio.switchAMFM(); // se cambia el estado inicial de la radio (FM-> false) a AM (true)
+        assertEquals(true, radio.getFrequency());
+        radio.switchAMFM(); // AM (true) a FM (false.)
+        assertEquals(false, radio.getFrequency());       
+    }
+}
